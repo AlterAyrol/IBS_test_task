5bd4afb42116d51c265f3231783440f8463e0c7e
AlterAyrol <lazarev.reach@gmail.com>
2023-09-22 20:25:34 +0300

    изменил url у части тестов

--- utils/api.py
+++ utils/api.py
@@ -1,21 +1,21 @@
 import requests
 
 
-base_url = 'https://rahulshettyacademy.com'     #Базовая URL
+base_url = 'https://reqres.in'     #Базовая URL
 
 
 class ReqresAPI:
 
     @staticmethod
     def get_list_users(page_key):
-        url = base_url + '/api/users?' + page_key
+        url = f'{base_url}/api/users?page={page_key}'
         result_get = requests.get(url=url)
         return result_get
 
 
     @staticmethod
-    def get_single_user(user_id):
-        url = base_url + '/api/users/' + user_id
+    def get_simple_user(user_id):
+        url = f'{base_url}/api/users/{user_id}'
         result_get = requests.get(url=url)
         return result_get
 
@@ -29,7 +29,7 @@ class ReqresAPI:
 
     @staticmethod
     def get_single_unknown(unknown_id):
-        url = base_url + '/api/unknown/' + unknown_id
+        url = f'{base_url}/api/unknown/{unknown_id}'
         result_get = requests.get(url=url)
         return result_get
 
@@ -43,22 +43,22 @@ class ReqresAPI:
 
     @staticmethod
     def update_by_put_user_info(user_id, body):
-        url = base_url + '/api/users/' + user_id
+        url = f'{base_url}/api/users/{user_id}'
         result_put = requests.put(url=url, json=body)
         return result_put
 
 
     @staticmethod
     def update_by_patch_user_info(user_id, body):
-        url = base_url + '/api/users/' + user_id
+        url = f'{base_url}/api/users/{user_id}'
         result_put = requests.patch(url=url, json=body)
         return result_put
 
 
     @staticmethod
     def delete_user(user_id):
-        url = base_url + '/api/users/' + user_id
-        result_post = requests.post(url=url)
+        url = f'{base_url}/api/users/{user_id}'
+        result_post = requests.delete(url=url)
         return result_post
 
 
@@ -78,6 +78,6 @@ class ReqresAPI:
 
     @staticmethod
     def get_list_users_with_delay(delay):
-        url = base_url + f'/api/users?delay={delay}'
+        url = f'{base_url}/api/users?delay={delay}'
         result_get = requests.get(url=url)
         return result_get
