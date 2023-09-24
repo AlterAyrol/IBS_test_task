1163b12b952b5549cf1086b6ef65035549de9864
AlterAyrol <lazarev.reach@gmail.com>
2023-09-24 15:52:12 +0300

    firefox

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,8 @@ import os
 import pytest
 from selene import browser
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as COptions
+from selenium.webdriver.firefox.options import Options as FOptions
 from dotenv import load_dotenv
 
 from utils import attach
@@ -14,33 +15,44 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture()
-def web_browser():
-    # driver_options = webdriver.ChromeOptions()
-    # driver_options.add_argument('--headless')
-    # browser.config.driver_options = driver_options
-    browser.config.window_width = 1600
-    browser.config.window_height = 1200
+@pytest.fixture(scope='function')
+def web_browser(request):
+
     browser.config.base_url = 'https://reqres.in'
-    options = Options()
+
+    # options = COptions()
+    # selenoid_capabilities = {
+    #     "browserName": "chrome",
+    #     "browserVersion": "100.0",
+    #     "selenoid:options": {
+    #         "enableVNC": True,
+    #         "enableVideo": True
+    #     }
+    # }
+    # options.capabilities.update(selenoid_capabilities)
+
+    options = FOptions()
     selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserName": "firefox",
+        "browserVersion": "97.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
+    options.capabilities.update(selenoid_capabilities)
 
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
 
-    options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
         command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     browser.config.driver = driver
+
+    # driver_options = webdriver.ChromeOptions()
+    # browser.config.driver_options = driver_options
 
     driver = browser.open('/')
 
@@ -53,3 +65,15 @@ def web_browser():
 
     browser.quit()
 
+
+@pytest.fixture(scope='function', params=[
+    pytest.param([1920, 1080], id='FullHD'),
+    pytest.param([2560, 1440], id='2k'),
+    pytest.param([4096, 2160], id='4k'),
+    pytest.param([800, 600], id='Small')
+])
+def window_size(request):
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
+
+    yield
